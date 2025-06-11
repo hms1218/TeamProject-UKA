8260ae8a3780ca8d558c9b625132177dc646b29b
isak9975 <isak9975@gmail.com>
2025-06-12 00:10:41 +0900

    틀제작3

--- src/components/DetailPage/DetailBody.js
+++ src/components/DetailPage/DetailBody.js
@@ -21,47 +21,105 @@ export const DetailBody = () => {
         <div className="container">
             <div className="top">
                 <div className="map">
-                    {/* <KoreaMapSection 
+                    <KoreaMapSection 
                                     // onRegionSelect={handleRegionSelect}
                                     //  selectedRegionId={selectedRegionId}
                                     //  tooltipContent={tooltipContent}
                                     //  setTooltipContent={setTooltipContent}
                                     //  regionInfo={regionInfo}
-                    /> */}
+                    />
                 </div>
 
                 <div className="combobox">
                     {/* 시 */}
-                    <select>
-                        
-                    {<option value=''></option>}
-                    
-                    </select>
+                    <div style={{display:'flex', flexDirection:'column'}}>
+                        <label className="text" for="si" >시 선택</label> 
+                        <select id='si'title="시입니다">                        
+                        {<option value={si}>시</option>}
+                        </select>
+                    </div>
 
                     {/* 군 */}
-                    <select>
-                        <option value=''></option>
-                    </select>
+                    <div style={{display:'flex', flexDirection:'column'}}>
+                        <label className="text" for="gun" >군 선택</label>
+                        <select>
+                            <option id="gun" value={gun}>군</option>
+                        </select>
+                    </div>
 
                     {/* 구 */}
+                    <div style={{display:'flex', flexDirection:'column'}}>
+                        <label className="text" for="gu" >구 선택</label>
+                        <select>
+                            <option id='gu' value={gu}>구</option>
+                        </select>
+                    </div>
+                </div>
+                
+            </div>{/* end top */}
+{/* ================================================================================================ */}
+            <div className="bottom">
+                {/*  */}
+                <div className="dropdown">
                     <select>
-                        <option value=''></option>
+                        <option value='8'>8</option>
+                        <option value='12'>12</option>
+                        <option value='16'>16</option>
                     </select>
                 </div>
-            </div>{/* end top */}
 
-            <div className="bottom">
+
                 {/* 상세정보 하나 들어가는 박스 */}
-                <div className="detail">
-                {/* 이미지 */}
-                <img src={img} alt="테스트 이미지"/>
-                
-                <hr/>
-                {/* 간략한 정보(하이퍼 링크) */}
-                <h3><a href="/">간략한 정보(하이퍼링크)</a></h3>
-                    
+                <div className="detail-box">
+                    <div className="detail">
+                    {/* 이미지 */}
+                        <img src={img} alt="테스트 이미지"/>
+                        
+                        <div className="detail-content">
+                            {/* 간략한 정보(하이퍼 링크) */}
+                            <h3><a href="/select">간략한 정보(하이퍼링크)</a></h3>
+                        </div>
+                    </div>
+
+                    {/* 상세정보 하나 들어가는 박스 */}
+                    <div className="detail">
+                    {/* 이미지 */}
+                        <img src={img} alt="테스트 이미지"/>
+                        
+                        
+                        <div className="detail-content">
+                            {/* 간략한 정보(하이퍼 링크) */}
+                            <h3><a href="/">간략한 정보(하이퍼링크)</a></h3>
+                        </div>
+                    </div>
+
+
+                    {/* 상세정보 하나 들어가는 박스 */}
+                    <div className="detail">
+                    {/* 이미지 */}
+                        <img src={img} alt="테스트 이미지"/>
+                        
+                        
+                        <div className="detail-content">
+                            {/* 간략한 정보(하이퍼 링크) */}
+                            <h3><a href="/">간략한 정보(하이퍼링크)</a></h3>
+                        </div>
+                    </div>
+
+                    {/* 상세정보 하나 들어가는 박스 */}
+                    <div className="detail">
+                    {/* 이미지 */}
+                        <img src={img} alt="테스트 이미지"/>
+                        
+                        
+                        <div className="detail-content">
+                            {/* 간략한 정보(하이퍼 링크) */}
+                            <h3><a href="/">간략한 정보(하이퍼링크)</a></h3>
+                        </div>
+                    </div>
                 </div>
-            </div> 
+            </div>{/* end bottom */}
+            
 
         </div>
     )

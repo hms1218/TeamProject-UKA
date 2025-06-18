f56e5bcb95870e9888312d9893518da08a4062ed
isak9975 <isak9975@gmail.com>
2025-06-18 23:28:36 +0900

    부트스트랩 정상화

--- FrontEnd/src/components/DetailPage/DetailBody.js
+++ FrontEnd/src/components/DetailPage/DetailBody.js
@@ -4,10 +4,6 @@ import KoreaMapSection from "../Map/KoreaMapSection";
 import './DetailBody.css'
 import { Link } from "react-router-dom";
 
-// 부트스트랩
-import {Carousel,Button, Card} from 'react-bootstrap'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { CardComponent } from "./CardComponent";
 
 export const DetailBody = () => {
@@ -26,8 +22,8 @@ export const DetailBody = () => {
 
 
     return(
-        <div className="container">
-            <div className="top">
+        <div className="DBcontainer">
+            <div className="DBtop">
                 <div className="map">
                     <KoreaMapSection 
                         // onRegionSelect={handleRegionSelect}
@@ -38,10 +34,10 @@ export const DetailBody = () => {
                     />
                 </div>
 
-                <div className="combobox">
+                <div className="DBcombobox">
                     {/* 시 */}
                     <div style={{display:'flex', flexDirection:'column'}}>
-                        <label className="text" for="si" >시 선택</label> 
+                        <label className="DBtext" for="si" >시 선택</label> 
                         <select id='si'title="시입니다">                        
                         {<option value={si}>시</option>}
                         </select>
@@ -49,7 +45,7 @@ export const DetailBody = () => {
 
                     {/* 군 */}
                     <div style={{display:'flex', flexDirection:'column'}}>
-                        <label className="text" for="gun" >군 선택</label>
+                        <label className="DBtext" for="gun" >군 선택</label>
                         <select>
                             <option id="gun" value={gun}>군</option>
                         </select>
@@ -57,7 +53,7 @@ export const DetailBody = () => {
 
                     {/* 구 */}
                     <div style={{display:'flex', flexDirection:'column'}}>
-                        <label className="text" for="gu" >구 선택</label>
+                        <label className="DBtext" for="gu" >구 선택</label>
                         <select>
                             <option id='gu' value={gu}>구</option>
                         </select>
@@ -67,56 +63,37 @@ export const DetailBody = () => {
             </div>{/* end top */}
 {/* ================================================================================================ */}
             {/* 지역선택하면 값을 받아서 렌더링하게 설정할것(지금은 일단 보임.) */}
-            <div className="bottom">
+            <div className="DBbottom">
                 {/* 렌더링 개수 정하는 드롭다운. */}
-                <div className="dropdown">
+                <div className="DBdropdown">
                     <select>
-                        <option value='8'>8</option>
-                        <option value='12'>12</option>
-                        <option value='16'>16</option>
+                        <option value='8'>세로</option>
+                        <option value='12'>가로</option>
                     </select>
                 </div>
 
 
                 {/* 상세정보 하나 들어가는 박스 */}
-                <div className="detail-box">
+                <div className="DBdetail-box">
 
                     {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-                    {/* 상세정보하나 */}
                     <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
-
-
-                    {/* 상세정보 하나 들어가는 박스 */}
-                    {/* <div className="detail"> */}
-                    {/* 이미지 */}
-                        {/* <img src={img} alt="테스트 이미지"/> */}
-                        
-                        {/* <div className="detail-content"> */}
-                            {/* 간략한 정보(하이퍼 링크) */}
-                            {/* <h3><Link to="/about/select">간략한 정보(하이퍼링크O)</Link></h3> */}
-                        {/* </div> */}
-                    {/* </div> */}
+                    <CardComponent img={img} description={'간략한 정보'} title={'제목'}/>
 
                     
 

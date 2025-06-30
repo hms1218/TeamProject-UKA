a57b5b04708405f2f2f89c0a9dfcc18c0c3e93c1
minkyung22 <jdg9926@naver.com>
2025-06-30 17:55:34 +0900

    250630 커밋
    집에 가자~

--- BackEnd/UKA/src/main/java/com/teamProject/UKA/auth/dto/SignupRequest.java
+++ BackEnd/UKA/src/main/java/com/teamProject/UKA/auth/dto/SignupRequest.java
@@ -19,11 +19,11 @@ public class SignupRequest {
 		@Email - 이메일 형식 검증
 	*/
 	@NotBlank(message = "username must not be blank")
-	@Size(min = 2, max = 20, message = "userId length must be between 2 and 20")
+	@Size(min = 6, max = 16, message = "userId length must be between 6 and 16")
 	private String userId;
 	
 	@NotBlank(message = "username must not be blank") 
-	@Size(min = 2, max = 10, message = "nickname length must be between 2 and 10")
+	@Size(min = 6, max = 16, message = "nickname length must be between 6 and 16")
 	private String nickname;
 	
     @NotBlank(message = "email must not be blank")
@@ -31,6 +31,6 @@ public class SignupRequest {
 	private String email;
     
     @NotBlank(message = "password must not be blank")
-    @Size(min = 6, message = "password must be at least 6 characters long")
+    @Size(min = 8, message = "password must be at least 8 characters long")
 	private String password;
 }

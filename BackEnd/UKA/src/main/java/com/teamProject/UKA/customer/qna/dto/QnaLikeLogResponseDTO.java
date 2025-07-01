32693f7a343149c1947e78f3e4063c52d3e06c70
ChoiKeen <chbk9195@naver.com>
2025-07-01 23:10:16 +0900

    수정완

--- BackEnd/UKA/src/main/java/com/teamProject/UKA/customer/qna/dto/QnaLikeLogResponseDTO.java
+++ BackEnd/UKA/src/main/java/com/teamProject/UKA/customer/qna/dto/QnaLikeLogResponseDTO.java
@@ -4,13 +4,16 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
+@Getter
+@Setter
 public class QnaLikeLogResponseDTO {
 	private Long id;
     private Long qnaNo;
     private String userId;
     private LocalDateTime createdAt;
-    private boolean isLikedByMe;
 }
